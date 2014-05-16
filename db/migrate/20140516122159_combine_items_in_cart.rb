38e7bfcb1f7ce2e5ee3c0840f124c859857249af
aleksandar-smiljanic <sax@sinapsi.com>
2014-05-16 17:58:40 +0200

    migrate

--- db/migrate/20140516122159_combine_items_in_cart.rb
+++ db/migrate/20140516122159_combine_items_in_cart.rb
@@ -1,17 +1,14 @@
 class CombineItemsInCart < ActiveRecord::Migration
-  def change
-  end
 
   def up
     # replace multiple items in cart with one + quantity
     Cart.all.each do |cart|
       #count repeated items
       sums = cart.line_items.group(:product_id).sum(:quantity)
-
       sums.each do |product_id, quantity|
         if quantity > 1
           #remove items
-          cart.line_items.where(product_d: product_id).delete_all
+          cart.line_items.where(product_id: product_id).delete_all
 
           # replace with single line
           item = cart.line_items.build(product_id: product_id)
@@ -19,6 +16,19 @@ class CombineItemsInCart < ActiveRecord::Migration
           item.save!
         end
       end
+    end
+  end
+
+  def down
+# split items with quantity>1 into multiple items
+    LineItem.where("quantity>1").each do |line_item|
+# add individual items
+      line_item.quantity.times do
+        LineItem.create cart_id: line_item.cart_id,
+                        product_id: line_item.product_id, quantity: 1
+      end
+# remove original item
+      line_item.destroy
     end
   end
 end
